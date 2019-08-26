9d12b772198d1c4a9e2d6e76438cdaec5546741b
zeL <3DenzeL@protonmail.com>
2019-08-26 02:36:41 -0400

    first commit for Capstone #4

---
+++ taskListRevistedCapstone4/Controllers/TaskListController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using taskListRevistedCapstone4.Models;
+
+namespace taskListRevistedCapstone4.Controllers
+{
+    [Authorize]
+    public class TaskListController : Controller
+    {
+        private readonly TaskListDbContext _context;
+
+        public TaskListController(TaskListDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(UserTask newTask)
+        {
+
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            newTask.UserId = id;
+            _context.UserTask.Add(newTask);
+            _context.SaveChanges();
+            return RedirectToAction("ViewTask");
+        }
+
+        public IActionResult DeleteTask(int Id)
+        {
+            UserTask found = _context.UserTask.Find(Id);
+            if(found != null)
+            {
+                _context.UserTask.Remove(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("ViewTask");
+        }
+
+        public IActionResult ViewTask()
+        {
+            List<UserTask> taskList = _context.UserTask.ToList();
+            return View(taskList);
+        }
+
+
+    }
+}
---
+++ taskListRevistedCapstone4/Models/UserTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace taskListRevistedCapstone4.Models
+{
+    public partial class UserTask
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool Complete { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
